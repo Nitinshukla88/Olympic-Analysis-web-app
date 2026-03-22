a9ba04666b67e64bf84f5662d3a327556ea12eb4
nitinshukla <nitin.shukla12004@gmail.com>
2026-03-23 00:00:20 +0530

    refactored the code

--- helper.py
+++ helper.py
@@ -1,6 +1,6 @@
 def medal_telly(df):
-        medal_telly = df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Medal', 'Event'])
-        x = medal_telly.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Gold',
+        medals = df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Medal', 'Event'])
+        x = medals.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Gold',
                                                                                        ascending=False).reset_index()
         x['total'] = x['Gold'] + x['Silver'] + x['Bronze']
         return x
