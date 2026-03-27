31c4c2f67c1bebd1b9e39bbd89488f11421899f7
nitinshukla <nitin.shukla12004@gmail.com>
2026-03-27 12:04:14 +0530

    year-wise tally created

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ df = pd.read_csv('athlete_events.csv')
 region_df = pd.read_csv('noc_regions.csv')
 
 df = preprocessor.preprocess(df, region_df)
+st.sidebar.title('Olympics Analysis')
 user_menu = st.sidebar.radio(
     'Select an Option',
     ('Medal Tally', 'Overall Analysis', 'Country-wise Analysis', 'Athlete-wise Analysis')
@@ -14,5 +15,9 @@ user_menu = st.sidebar.radio(
 st.dataframe(df)
 
 if user_menu == 'Medal Tally':
+    st.sidebar.header('Medal Tally')
+    years, country = helper.country_year_list(df)
+    selected_year = st.sidebar.selectbox('Select Year', years)
+    selected_country = st.sidebar.selectbox('Select Country', country)
     medal_telly = helper.medal_telly(df)
     st.dataframe(medal_telly)
--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+import numpy as np
 def medal_telly(df):
         medals = df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Medal', 'Event'])
         x = medals.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Gold',
@@ -6,5 +7,14 @@ def medal_telly(df):
         x['Gold'] = x['Gold'].astype(int)
         x['Silver'] = x['Silver'].astype(int)
         x['Bronze'] = x['Bronze'].astype(int)
-        x['Total'] = x['Total'].astype(int)
+        x['total'] = x['total'].astype(int)
         return x
+
+def country_year_list(df):
+    years = df['Year'].unique().tolist()
+    years.sort()
+    years.insert(0, 'Overall')
+    country = np.unique(df['region'].dropna().values).tolist()
+    country.sort()
+    country.insert(0, 'Overall')
+    return years, country
